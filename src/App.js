1346cbe7713359f0bc5f282aa2f13943d04f0784
김정민 <dsaqaz421@naver.com>
2020-12-04 15:26:32 +0900

    building the router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import About from "./routes/About";
+import Home from "./routes/Home";
 
-function App(){
-  return <sapn>something</sapn>;
+/* exact 옵션을 넣어서 해당 path가 아니면 렌더링을 안하도록 설정 */
+function App() {
+  return (
+    <HashRouter>
+      <Route path="/" exact={true} component={Home} />  
+      <Route path="/about" component={About} />
+    </HashRouter>
+  );
 }
 
 export default App;
