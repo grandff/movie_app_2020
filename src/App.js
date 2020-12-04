54ba4004b1d1744bca4b6e17f5b2db2141cd472c
김정민 <dsaqaz421@naver.com>
2020-12-04 12:28:00 +0900

    styling

--- src/App.js
+++ src/App.js
@@ -1,37 +1,65 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
 
-class App extends React.Component{
+class App extends React.Component {
   state = {
-    isLoading : true,  // 로딩 환경
-    movies : []
+    isLoading: true, // 로딩 환경
+    movies: [],
   };
 
-  getMovies = async() => {
+  getMovies = async () => {
     // data load. fetch 대신 axios 사용
-    // axios 속도가 그리 빠른편이 아니므로 didmount가 끝날때까지 시간이 걸린다라는 명시를 해줘야함.     
+    // axios 속도가 그리 빠른편이 아니므로 didmount가 끝날때까지 시간이 걸린다라는 명시를 해줘야함.
     // 즉 비동기 처리. axios가 끝날때 까지 기다렸다가 계속함
     // const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json"); bad
-    const {data : { data : { movies }}} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");  // es6
-    
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    ); // es6
+
     //this.setState({movies : movies}); // bad
-    this.setState({movies, isLoading : false});  // es6
-  }
+    this.setState({ movies, isLoading: false }); // es6
+  };
 
   // component 완료 후 작업. 대표적으로 fetch data가 필요함
-  componentDidMount(){
+  componentDidMount() {
     //setTimeout(() => {
     //  this.setState({ isLoading : false})
     //}, 6000);
-    this.getMovies()    
-  };
+    this.getMovies();
+  }
 
-  render(){
+  render() {
     const { isLoading, movies } = this.state; // es6
-    return <div>{isLoading ? "Loading..." : movies.map(movie => {      
-      return <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} />
-    })}</div>;
+    return (
+      <section class="container">
+        {isLoading
+          ? (
+            <div class="loader">
+              <span class="loader__text">Loading...</span>
+            </div>
+          )
+          : (
+            <div class="movies">
+              {movies.map( movie =>
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                />              
+              )}
+            </div>
+          )}
+      </section>
+    );
   }
 }
 
--- src/Movie.js
+++ src/Movie.js
@@ -1,19 +1,29 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
+import "./Movie.css";
 
 // state가 필요없는 경우에는 class 대신 function을 쓰면 됨
 // movie를 render 하기 위한 파일
-function Movie({id, year, title, summary, poster}){
-    return <h5>{title}</h5>
+function Movie({ id, year, title, summary, poster }) {
+  return (
+    <div class="movie">
+      <img src={poster} alt={title} title={title} />
+      <div class="movie__data">
+        <h3 class="movie__title">{title}</h3>
+        <h5 class="movie__year">{year}</h5>
+        <p class="movie__summary">{summary}</p>
+      </div>
+    </div>
+  );
 }
 
 // proptypes 설정
 Movie.propTypes = {
-    id : PropTypes.number.isRequired,
-    year : PropTypes.number.isRequired,
-    title : PropTypes.string.isRequired,
-    summary : PropTypes.string.isRequired,
-    poster : PropTypes.string.isRequired
+  id: PropTypes.number.isRequired,
+  year: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
 };
 
 export default Movie;
